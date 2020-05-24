d2dca7c6a332a6717bb194a56574ca226ba0cd36
janggalman <janggalman@gmail.com>
2020-05-24 14:34:57 +0900

    두번째 버튼 동작 연습문제 풀이

--- app/src/main/java/kr/tjoeun/androidbasic01_intent_20200524/MainActivity.kt
+++ app/src/main/java/kr/tjoeun/androidbasic01_intent_20200524/MainActivity.kt
@@ -17,5 +17,14 @@ class MainActivity : AppCompatActivity() {
             startActivity(myIntent)
 
         }
+
+//        두번째 액티비티(SecondActivity)로 버튼을 누르면 이동, 돌아가기 누르면
+//        다시 메인으로 돌아오도록
+        goToSecondBtn.setOnClickListener {
+
+            val myIntent = Intent(this, SecondActivity::class.java)
+            startActivity(myIntent)
+        }
+
     }
 }
